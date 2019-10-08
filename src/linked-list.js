629154418d373a415d637ed804139a5cd22fdca1
SergeyLysenko <sergey.s.lysenko@sms-a.ru>
2019-10-08 21:30:23 +0400

    feat: add task

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,27 +1,71 @@
 const Node = require('./node');
 
 class LinkedList {
-    constructor() {}
+    constructor() {
+        this.myList = []
+    }
 
-    append(data) {}
+    append(data) {
+        this.myList.push(new Node(data))
+        return this
+    }
 
-    head() {}
+    head() {
+        if (this.myList.length != 0) {return this.myList[0].data}
+        else {return null}
+    }
 
-    tail() {}
+    tail() {
+        if (this.myList.length != 0) {return this.myList[this.myList.length - 1].data}
+        else {return null}
+    }
 
-    at(index) {}
+    at(index) {
+        if (this.myList.length != 0 && index>=0 && index<this.myList.length) {return this.myList[index].data}
+        else {return null}
+    }
 
-    insertAt(index, data) {}
+    insertAt(index, data) {
+        this.myList.splice(index, 0, new Node(data))
+    }
 
-    isEmpty() {}
+    isEmpty() {
+        return this.myList.length == 0
+    }
 
-    clear() {}
+    clear() {
+        this.myList.length = 0
+        return this
+    }
 
-    deleteAt(index) {}
+    deleteAt(index) {
+        this.myList.splice(index, 1)
+        return this
+    }
 
-    reverse() {}
+    reverse() {
+        this.myList.reverse()
+        return this
+    }
 
-    indexOf(data) {}
+    indexOf(data) {
+        for (let index = 0; index < this.myList.length; index++) {
+            if(this.myList[index].data == data) return index
+        }
+        return -1
+    }
+    //bc myList get underfined vice 0
+    get length() {
+        return this.myList.length
+    }
+    // like any
+    get _tail() {
+        return this.myList[this.myList.length - 1];
+    }
+    get _head() {
+        return this.myList[0];
+    }
+
 }
 
 module.exports = LinkedList;
